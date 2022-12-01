4cad32a3d51692effaa8a27d7d836f8432c1ab58
Andrei Varzar <43003757+wizzandrew@users.noreply.github.com>
2022-12-01 22:01:23 +0100

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <nav>
+      <RouterLink to="/">Home</RouterLink> |
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+    <RouterView />
+  </div>
+</template>
+
+<style>
+.app {
+  text-align: center;
+}
+</style>
